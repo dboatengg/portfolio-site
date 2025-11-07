01c5773e6eb137756a639dd7c4a6e68f3ab8b7fa
Dickson Boateng <dicksonboateng67@gmail.com>
2025-11-07 17:01:23 +0000

    Update profile summary

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,12 +4,10 @@ export default function Intro() {
       <h1 className="text-4xl font-semibold text-white mb-1">
         Dickson Boateng
       </h1>
-      <p className="text-gray-400 mb-4">Full-Stact Developer </p>
+      <p className="text-gray-400 mb-4">Frontend Developer </p>
 
       <p className="text-gray-300 leading-relaxed max-w-2xl">
-        I build polished and performant web experiences with thoughtful details
-        and intuitive design. Passionate about front-end development, user
-        experience, and creative problem-solving.
+        I build with: Next.js • React.js • JavaScript • TypeScript • Tailwind CSS • WordPress
       </p>
     </section>
   )
