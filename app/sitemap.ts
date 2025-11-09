2c030d90383d1fab43059801af61ad3c51a7eaa3
Dickson Boateng <dicksonboateng67@gmail.com>
2025-11-09 12:45:33 +0000

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,43 @@
+// import { MetadataRoute } from "next";
+// import { allBlogs } from "@/.contentlayer/generated";
+
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//   const posts = allBlogs
+
+//   return [
+//     {
+//       url: "https://dicksonboateng.com",
+//       lastModified: new Date(),
+//     },
+//     ...posts.map((post) => ({
+//       url: `https://dicksonboateng.com/blog/${post.slug}`,
+//       lastModified: post.date,
+//     })),
+//   ];
+// }
+
 // app/sitemap.ts
 import { MetadataRoute } from "next";
 import { allBlogs } from "@/.contentlayer/generated";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const posts = allBlogs
+  const baseUrl = "https://dicksonboateng.com";
 
-  return [
-    {
-      url: "https://dicksonboateng.com",
-      lastModified: new Date(),
-    },
-    ...posts.map((post) => ({
-      url: `https://dicksonboateng.com/blog/${post.slug}`,
-      lastModified: post.date,
-    })),
-  ];
+  const posts = allBlogs.map((post) => ({
+    url: `${baseUrl}/blog/${post.slug}`,
+    lastModified: post.date,
+  }));
+
+  const staticPages = [
+    "",
+    "about",
+    "projects",
+    "blog",
+  ].map((path) => ({
+    url: `${baseUrl}/${path}`,
+    lastModified: new Date(),
+  }));
+
+  // Combine
+  return [...staticPages, ...posts];
 }
