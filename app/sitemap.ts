f0d2718a4baf211b8fae980366d98b8f12c9c647
Dickson Boateng <dicksonboateng67@gmail.com>
2025-11-10 03:41:03 +0000

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,22 +1,3 @@
-// import { MetadataRoute } from "next";
-// import { allBlogs } from "@/.contentlayer/generated";
-
-// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-//   const posts = allBlogs
-
-//   return [
-//     {
-//       url: "https://dicksonboateng.com",
-//       lastModified: new Date(),
-//     },
-//     ...posts.map((post) => ({
-//       url: `https://dicksonboateng.com/blog/${post.slug}`,
-//       lastModified: post.date,
-//     })),
-//   ];
-// }
-
-// app/sitemap.ts
 import { MetadataRoute } from "next";
 import { allBlogs } from "@/.contentlayer/generated";
 
@@ -31,7 +12,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticPages = [
     "",
     "about",
-    "projects",
     "blog",
   ].map((path) => ({
     url: `${baseUrl}/${path}`,
