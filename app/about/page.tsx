1871fc12cf3b8e710e51ad6c649baf74636d81ef
Dickson Boateng <dicksonboateng67@gmail.com>
2025-11-15 09:55:37 +0000

    Update About me page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,38 +12,40 @@ export default function AboutPage() {
   return (
     <section className="max-w-3xl mx-auto py-10 space-y-8">
       <header>
-        <h1 className="text-3xl font-bold mb-3">About</h1>
+        <h1 className="text-3xl font-bold mb-2">About</h1>
       </header>
 
       <div>
-        <h2 className="text-xl font-semibold mb-2">Background</h2>
-        <p className="text-gray-300 dark:text-gray-400 mb-4">
-          Growing up with a personal computer at home sparked my curiosity about how computers work and what they can do. In high school, I did the General Science course hoping it would lead me to study Computer Science at university.</p>
-          <p className="text-gray-300 dark:text-gray-400 mb-4">However, when it came time for university admissions, I was offered a place in Occupational Therapy at the University of Ghana, a field quite different from what I had hoped for. Although it wasn't my first choice, I decided to embrace the opportunity and see where it would lead me. </p>
-        
-        <p className="text-gray-300 dark:text-gray-400 mb-2">
-          Still, my passion for computers never faded. While I was doing the degree, I kept learning about technology on my own, taking online courses, building small projects, and staying connected to the tech world. Even though my formal studies led me down a different path, my curiosity and persistence helped me find my way back to working with computers again.        </p>
-        <p className="text-gray-300 dark:text-gray-400 mb-4">
-          Looking back, I’m glad I never gave up on my interest in technology. Today, I’m grateful to do what I love. I’ve had the chance to collaborate with developers and designers from different backgrounds. My journey wasn’t straightforward, but the skills and experiences I gained along the way have made me a better problem-solver and teammate in the tech world.
+        <h2 className="text-xl font-semibold mb-4">Background</h2>
+        <p className="text-gray-300 dark:text-gray-300 mb-4 leading-relaxed">
+          I didn’t start my career in a traditional Computer Science program. I began my university journey studying Occupational Therapy at the University of Ghana, an unexpected detour from my original plan to pursue tech. 
           </p>
-        <p className="text-gray-300 dark:text-gray-400">
+          <p className="dark:text-gray-300 mb-4 leading-relaxed">But even while studying that degree, I kept learning about technology on my own, taking online courses, building small projects, and staying connected to the tech community.</p>
+        <p className="text-gray-300 dark:text-gray-300 mb-4 leading-relaxed">
+          That persistence eventually brought me back to software development, where I’ve been able to combine analytical thinking and empathy from my background with the technical skills I’ve built over the years. 
+        </p>
+        <p className="text-gray-300 dark:text-gray-300 mb-4 leading-relaxed">
+          Today, I work fully in web development and have had the privilege of collaborating with engineers and designers from diverse backgrounds.           
+        </p>
+        <p className="dark:text-gray-300 mb-4 leading-relaxed">The path here wasn’t linear, but it shaped me into a problem-solver who learns quickly, works well with others, and approaches challenges with both creativity and discipline.</p>
+        <p className="text-gray-300 dark:text-gray-300 leading-relaxed">
           When I’m not coding, I enjoy writing articles about web development and personal growth. Sharing my experiences and
             insights with the <span className="text-blue-500 underline"><Link href="https://x.com/search?q=alege_dev&src=typed_query" target="_blank">tech community</Link></span> is something I find incredibly rewarding.
         </p>
       </div>
       <div>
         <h2 className="text-xl font-semibold mb-4">What I'm doing now</h2>
-        <ul className="space-y-2 list-disc list-inside text-gray-300 dark:text-gray-400 marker:text-neutral-500 dark:marker:text-neutral-400">
+        <ul className="space-y-2 list-disc list-inside text-gray-300 dark:text-gray-300 marker:text-neutral-500 dark:marker:text-neutral-400">
             <li>Working as a full-time remote web developer</li>        
             <li>I run a clothing brand/store [at] <span className="text-blue-500 underline"><Link href="https://www.boldpixelsgh.com">boldpixelsgh.com</Link></span>.</li>
             <li>Working on occasional freelance projects </li>        
-            <li>Building personal projects with NextJS</li>
+            <li>Building production-ready side projects with NextJS</li>
             <li>Learning Nodejs</li>
         </ul>
       </div>
       <div>
         <h2 className="text-xl font-semibold mb-4">Fun Facts about me</h2>
-        <ul className="space-y-2 list-disc list-inside text-gray-300 dark:text-gray-400 marker:text-neutral-500 dark:marker:text-neutral-400">
+        <ul className="space-y-2 list-disc list-inside text-gray-300 dark:text-gray-300 marker:text-neutral-500 dark:marker:text-neutral-400">
             <li>I have never missed an episode from these podcasts: Darknet Diaries and What Now with Trevor Noah.</li>
             <li>I’ve watched every season of Black Mirror twice!</li>
             <li>I consider Pavel Durov (Telegram Founder & CEO) a mentor from afar. His discipline and philosophy deeply inspire me.</li>
@@ -52,7 +54,7 @@ export default function AboutPage() {
 
       <div>
         <h2 className="text-xl font-semibold mb-4">Publications</h2>
-        <ul className="space-y-2 list-disc list-inside text-gray-300 dark:text-gray-400 marker:text-neutral-500 dark:marker:text-neutral-400">
+        <ul className="space-y-2 list-disc list-inside text-gray-300 dark:text-gray-300 marker:text-neutral-500 dark:marker:text-neutral-400">
             <li className="text-blue-500 underline"><Link href="https://www.freecodecamp.org/news/asynchronous-programming-in-javascript/" target="_blank">Asynchronous Programming in JavaScript – Guide for Beginners</Link></li>
             <li className="text-blue-500 underline"><Link href="https://www.freecodecamp.org/news/context-api-in-react/" target="_blank">How to Use the React Context API in Your Projects</Link></li>
             <li className="text-blue-500 underline"><Link href="https://www.freecodecamp.org/news/javascript-dom/" target="_blank">How the Document Object Model Works in JavaScript</Link></li>
