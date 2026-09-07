0cc0ed7cc22ef39dff7e09c9bd1f0a2c8c7a084b
Dickson Boateng <dicksonboateng@proton.me>
2026-09-07 10:04:11 +0000

    Improve blog page subtitle

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -46,7 +46,7 @@ export default function BlogPage() {
           Blog
         </h1>
         <p className="text-sm md:text-base italic text-[rgb(var(--muted-text))] max-w-2xl">
-        I don&apos;t keep a diary. This blog is my diary. I want to keep writing so that in 5 years I can look back and marvel at how much I&apos;ve improved at putting my thoughts into words.
+        The goal is to keep writing so that in 5 years I can look back and marvel at how much I&apos;ve improved at putting my thoughts into words.
         </p>
       </header>
 
